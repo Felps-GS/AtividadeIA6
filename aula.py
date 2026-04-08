a9a44e039e25edce70ab99182ae7aa0e1beae8d8
Felps-GS <felipeg.desouza4@gmail.com>
2026-04-08 11:16:15 -0300

    Adicionando mais linhas de codigos

--- aula.py
+++ aula.py
@@ -1,11 +1,19 @@
+# ===============================================
+# Aula 06: Prática em Python 
+# Tema: Machine Learning - Classificação
+# Dataset: Iris
+# ===============================================
 
 
 import pandas as pd
+import numpy as np
+import matplotlib as plt
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
 iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -22,14 +30,33 @@ scarler = StandardScaler()
 X_train_scaled = scarler.fit_transform(X_train)
 X_test_scaled = scarler.transform(X_test)
 
+tree = DecisionTreeClassifier(max_depth=3, random_state=42)
+tree.fit(X_train_scaled, y_train)
+
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train_scaled, y_train)
 
-y_pred = knn.predict(X_test_scaled)
+y_pred_tree = tree.predict(X_test_scaled)
+y_pred_knn = knn.predict(X_test_scaled)
 
-print("\nAcurácia: ", accuracy_score(y_test, y_pred))
-print("\nRelatório de Classificação: ")
-print(classification_report(y_test, y_pred, target_names=iris.target_names))
+print("\nAcurácia Árvore de Decisão: ", accuracy_score(y_test, y_pred_tree))
+print("\nAcurácia KNN: ", accuracy_score(y_test, y_pred_knn))
 
-print("\nMatriz de Confusão: ")
-print(confusion_matrix(y_test, y_pred))
+cm_tree = confusion_matrix(y_test, y_pred_tree)
+disp_tree = ConfusionMatrixDisplay(confusion_matrix=cm_tree, display_labels=iris.target_names)
+disp_tree.plot()
+plt.title("Matriz de Confusão - Árvore de Decisão")
+plt.show()
+
+
+cm_knn = confusion_matrix(y_test, y_pred_knn)
+disp_knn = ConfusionMatrixDisplay(confusion_matrix=cm_knn, display_labels=iris.target_names)
+disp_knn.plot()
+plt.title("Matriz de Confusão - KNN")
+plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_boundary(X, y, model, title):
+    X = X[:, 0].min() - 1, X[:, 0].max() + 1
+
