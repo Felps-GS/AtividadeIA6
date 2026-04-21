b8d7c69cc6eada444788f2cecd1b42a5e22c7330
Felipe <felipeg.desouza4@gmail.com>
2026-04-21 11:21:05 -0300

    Atividade Finalizada

--- aula.py
+++ aula.py
@@ -58,5 +58,41 @@ plt.show()
 from matplotlib.colors import ListedColormap
 
 def plot_decision_boundary(X, y, model, title):
-    X = X[:, 0].min() - 1, X[:, 0].max() + 1
+    X = X[:, 0]
+    h = .02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(8, 6))
+    plt.contourf(xx, yy, Z, alpha=0.3, cmap=ListedColormap(('#FFAAAA', '#AAFFAA', '#AAAAFF')))
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=ListedColormap(('#FF0000', '#00FF00', '#0000FF')))
+    plt.xlabel(iris.feature_names[0])
+    plt.ylabel(iris.feature_names[1])
+    plt.title(title)
+    plt.show()
+
+X_vis = X.iloc[:, [:, :2]]
+X_train_vis, X_test_vis, y_train_vis, y_test_vis = train_test_split(X_vis, y, test_size=0.3, random_state=42, stratify=y)
+
+scarler_vis = StandardScaler()
+X_train_scaled_vis = scarler_vis.fit_transform(X_train_vis)
+X_test_scaled_vis = scarler_vis.transform(X_test_vis)
+
+tree_vis = DecisionTreeClassifier(max_depth=3, random_state=42)
+tree_vis.fit(X_train_scaled_vis, y_train_vis)
+
+knn_vis = KNeighborsClassifier(n_neighbors=5)
+knn_vis.fit(X_train_scaled_vis, y_train_vis)
+
+plot_decision_boundary(X_train_scaled_vis, y_train_vis, tree_vis, "Fronteira de Decisão - Árvore")
+plot_decision_boundary(X_train_scaled_vis, y_train_vis, knn_vis, "Fronteira de Decisão - KNN")
+
+
+
+
+    
 
